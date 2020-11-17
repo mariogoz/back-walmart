cb015e5776806b2aefe34583489be00505ba5ded
Mario Gonzalez Sanchez <mariogonzalez@MacBook-Air-de-Mario.local>
2020-11-17 03:10:58 -0300

    init merge

---
+++ src/controllers/products/index.js
@@ -0,0 +1,23 @@
+const products = () => ({
+    get: async (req, res) => {
+        const products = res.locals.result;
+        const isPalindromic = res.locals.isPalindromic;
+        let response = {
+          discount: isPalindromic,
+          products: []
+        }
+        products.forEach( ( {id, brand, description, image, price}) => {
+          response.products.push({
+            id,
+            brand,
+            description,
+            image,
+            price,
+            sale: isPalindromic ? price * 0.5 : price
+          });
+        });
+        res.status(200).send(response);
+    }
+});
+
+module.exports = products;
